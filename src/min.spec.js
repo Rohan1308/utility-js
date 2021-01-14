d575ce6a3689c338767f22d370d6b99bd447167f
Rohan Kumar <rokuog@gmail.com>
2021-01-14 22:59:21 +0530

    Fixed min test file

--- src/min.spec.js
+++ src/min.spec.js
@@ -3,12 +3,12 @@ const min = require('./min');
 
 describe('Min', () => {
 
-	it('Max of [] is []', () => {
-		expect(max([])).toEqual([]);
+	it('Min of [] is []', () => {
+		expect(min([])).toEqual([]);
 	});
 
-	it('Max of [1,3,8,5] is 1', () => {
-		expect(max([3,1,8,5])).toEqual(1);
+	it('Min of [1,3,8,5] is 1', () => {
+		expect(min([3,1,8,5])).toEqual(1);
 	});
 
 })
